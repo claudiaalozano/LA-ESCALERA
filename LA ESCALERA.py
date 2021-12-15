d72f0c55a808a10b7b3a716aeae532111e92de33
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-15 18:45:56 +0100

    siempre fuertes

--- LA ESCALERA.py
+++ LA ESCALERA.py
@@ -1,10 +1,37 @@
-TAMAÑO_ESCALERA = int(input("ELIGE EL TAMAÑO DE LA ESCALERA >>>  "))
+#TAMAÑO_ESCALERA = int(input("ELIGE EL TAMAÑO DE LA ESCALERA >>>  "))
 
-ESCALERA = []
-for i in range(TAMAÑO_ESCALERA):
-    ESCALERA.append([])
-    for j in range(TAMAÑO_ESCALERA):
-        ESCALERA[i].append("#")
+#ESCALERA = []
+#for i in range(TAMAÑO_ESCALERA):
+    #ESCALERA.append([])
+    #for j in range(TAMAÑO_ESCALERA):
+        #ESCALERA[i].append("#")
         #ESCALERA[i:TAMAÑO_ESCALERA] = ' '
 
-print(ESCALERA)
+#print(ESCALERA)
+from random import randint
+
+n = randint(5,10)
+
+matriz = [
+    ["a"],
+    ["a"],
+    ["a"],
+]
+
+matrix = []
+matrix.append("a")
+
+for k in range(n):
+    matrix = matrix.append("a")
+
+print(matrix)
+
+
+print(matrix)
+
+b = ""
+
+for k in range(3):
+    for j in range(1):
+        b += str(matriz[k][j])+"\t"
+    print(b)
