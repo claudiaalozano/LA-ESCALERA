bbb4b5cf7b38c7db17334d01c50f884fda0a2c4c
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 12:10:56 +0100

    código limpio

--- LA ESCALERA.py
+++ LA ESCALERA.py
@@ -1,18 +1,8 @@
-#TAMAÑO_ESCALERA = int(input("ELIGE EL TAMAÑO DE LA ESCALERA >>>  "))
 
-#ESCALERA = []
-#for i in range(TAMAÑO_ESCALERA):
-    #ESCALERA.append([])
-    #for j in range(TAMAÑO_ESCALERA):
-        #ESCALERA[i].append("#")
-        #ESCALERA[i:TAMAÑO_ESCALERA] = ' '
-
-#print(ESCALERA)
 from random import randint
 
 n = int(input("Elija el tamaño de la escalera:"))
 
-#matriz = [ ["a"], ["a"], ["a"]]
 
 matrix = []
 matrix.append("a" * n)
